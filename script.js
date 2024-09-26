b479a3587953bba7bd8da675faf8b0c5f0f45c28
tgkcilli <149543808+tgkcilli@users.noreply.github.com>
2024-09-26 00:30:38 -0300

    personalizacao + tentativa de function

---
+++ script.js
@@ -0,0 +1,13 @@
+function cadastrar() {
+    const placa = document.getElementById("placa").value;
+    const proprietario = document.getElementById("proprietario").value;
+    const apartamento = document.getElementById("apartamento").value;
+    const bloco  = document.getElementById("bloco").value;
+    const modelo = document.getElementById("modelo").value;
+    const cor  = document.getElementById("cor").value;
+    const vaga = document.getElementById("vaga").value;
+
+
+    console.log("Proprietário: " + proprietario + "\nApartamento: " + apartamento);
+
+}
